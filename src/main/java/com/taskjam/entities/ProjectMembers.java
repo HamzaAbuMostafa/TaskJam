c320978d35ad274dc96fad6164253b45fc25f155
Hamzah Aboumostafa <hnamostafa2007@gmail.com>
2026-08-19 14:22:48 +0300

    ProjectMembers entity created

---
+++ src/main/java/com/taskjam/entities/ProjectMembers.java
@@ -0,0 +1,28 @@
+package com.taskjam.entities;
+
+public class ProjectMembers {
+
+    private int projectId;
+    private int userId;
+
+    public ProjectMembers(int projectId, int userId) {
+        this.projectId = projectId;
+        this.userId = userId;
+    }
+
+    public int getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(int projectId) {
+        this.projectId = projectId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+}

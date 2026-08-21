118c672d33520b3d2903501912e70885f74e3d54
Hamzah Aboumostafa <hnamostafa2007@gmail.com>
2026-08-21 23:26:44 +0300

    Created ProjectRepository by inheriting Generic Repository class for cleaner code

---
+++ src/main/java/com/taskjam/repositories/ProjectRepository.java
@@ -0,0 +1,31 @@
+package com.taskjam.repositories;
+
+import com.taskjam.entities.Project;
+import com.taskjam.mappers.ProjectSupplier;
+
+import java.sql.Connection;
+import java.util.Optional;
+
+public class ProjectRepository extends GenericRepository<Project> {
+
+    public Optional<Project> getProjectById(int id){
+        String sql = "SELECT * FROM projects WHERE id = ?";
+        return executeQuery(sql, ProjectSupplier::getProject, id);
+    }
+
+    public  Optional<Project> getProjectByName(String name){
+        String sql = "SELECT * FROM projects WHERE name = ?";
+        return executeQuery(sql, ProjectSupplier::getProject, name);
+    }
+
+    public int addProject(Project project){
+        String sql = "INSERT INTO projects (name,description,created_by,created_at) VALUES (?,?,?,?)";
+        return executeUpdateReturnGeneratedKey(sql,
+                project.getName(),project.getDescription(),project.getCreatedBy(),project.getCreatedAt());
+    }
+
+    public boolean deleteProjectById(int id){
+        String sql = "DELETE FROM projects WHERE id = ?";
+        return executeUpdate(sql,id);
+    }
+}

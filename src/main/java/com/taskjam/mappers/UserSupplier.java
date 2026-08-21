2d02f2b49fe9d9a16c0d0332e6bafc5e3143d51f
Hamzah Aboumostafa <hnamostafa2007@gmail.com>
2026-08-21 23:07:16 +0300

    Created a supplier that returns User instances

---
+++ src/main/java/com/taskjam/mappers/UserSupplier.java
@@ -0,0 +1,19 @@
+package com.taskjam.mappers;
+
+import com.taskjam.entities.User;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+
+public class UserSupplier {
+    public static User getUser(ResultSet resultSet) throws SQLException {
+        return new User(
+                resultSet.getInt("id"),
+                resultSet.getString("username"),
+                resultSet.getString("email"),
+                resultSet.getString("password_hash"),
+                resultSet.getObject("created_at", LocalDateTime.class)
+                );
+    }
+}

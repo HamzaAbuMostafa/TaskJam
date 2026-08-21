982db6e72add2ba7b51ed4c8b7bb870c51610912
Hamzah Aboumostafa <hnamostafa2007@gmail.com>
2026-08-21 23:29:57 +0300

    Created a Supplier that returns ProjectMembers instances

---
+++ src/main/java/com/taskjam/mappers/ProjectMembersSupplier.java
@@ -0,0 +1,15 @@
+package com.taskjam.mappers;
+
+import com.taskjam.entities.ProjectMembers;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ProjectMembersSupplier {
+    public ProjectMembers getProjectMembers(ResultSet rs) throws SQLException{
+        return new ProjectMembers(
+                rs.getInt("project_id"),
+                rs.getInt("user_id")
+        );
+    }
+}

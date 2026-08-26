96a786c9945655d18f1ee88bfa70ecc5d41a7800
Hamzah Aboumostafa <hnamostafa2007@gmail.com>
2026-08-26 20:21:44 +0300

    Creation of LoginServlet that implements doGet that allows the users to logout from the acc and redirect them to the login page

---
+++ src/main/java/com/taskjam/servlet/LogoutServlet.java
@@ -0,0 +1,24 @@
+package com.taskjam.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet("/logoutServlet")
+public class LogoutServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+        if(session != null)
+            session.invalidate();
+        String message = java.net.URLEncoder.encode("You have been logged out", StandardCharsets.UTF_8);
+        response.sendRedirect("login.jsp?success=" + message);
+    }
+}

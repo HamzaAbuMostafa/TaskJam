245c28e9017f81fae0bef656a85b13e099339b4e
Hamzah Aboumostafa <hnamostafa2007@gmail.com>
2026-08-26 20:21:08 +0300

    Creation of LoginServlet that implements doPost it allows users to login into their acc and redirects them to the dashboard.jsp if successful

---
+++ src/main/java/com/taskjam/servlet/LoginServlet.java
@@ -0,0 +1,45 @@
+package com.taskjam.servlet;
+
+import com.taskjam.entity.User;
+import com.taskjam.repository.UserRepository;
+import com.taskjam.service.UserService;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
+
+@WebServlet("/loginServlet")
+public class LoginServlet extends HttpServlet {
+    private UserService userService;
+
+    @Override
+    public void init(){
+        userService = new UserService(new UserRepository());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+
+        Optional<User> user = userService.authenticateUser(email, password);
+        if(user.isPresent()){
+            int id = user.get().getId();
+            HttpSession session = request.getSession();
+            session.setAttribute("userId",id);
+            response.sendRedirect("dashboard.jsp");
+        } else {
+            String errorMessage = URLEncoder.encode("Incorrect email or password.", StandardCharsets.UTF_8);
+            response.sendRedirect("login.jsp?error=" + errorMessage);
+        }
+    }
+}

1c2123f81f5e4e6ab4775917f6e221193fa3b91c
Hamzah Aboumostafa <hnamostafa2007@gmail.com>
2026-08-31 19:50:07 +0300

    Adding a private projectMembersRepository. replacing the addProject method because it used to add the project to the project table in database without adding it to the project_members table too. The new method createProjectAndAssignOwner that has transaction management in which the database doesnt commit unless both adding to project table and project_members table execute successfully

--- src/main/java/com/taskjam/service/ProjectService.java
+++ src/main/java/com/taskjam/service/ProjectService.java
@@ -1,30 +1,54 @@
 package com.taskjam.service;
 
+import com.taskjam.config.DatabaseConnectionManager;
 import com.taskjam.entity.Project;
+import com.taskjam.entity.ProjectMembers;
+import com.taskjam.entity.User;
+import com.taskjam.repository.ProjectMembersRepository;
 import com.taskjam.repository.ProjectRepository;
 import com.taskjam.repository.UserRepository;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class ProjectService {
 
      private final ProjectRepository projectRepo;
      private final UserRepository userRepo;
+     private final ProjectMembersRepository projectMembersRepo;
 
-    public ProjectService(ProjectRepository projectRepo, UserRepository userRepo) {
+    public ProjectService(ProjectRepository projectRepo, UserRepository userRepo,ProjectMembersRepository projectMembersRepo) {
         this.projectRepo = projectRepo;
         this.userRepo = userRepo;
+        this.projectMembersRepo = projectMembersRepo;
     }
 
-    public int addProject(Project project){
-        if(project == null)
-            throw new IllegalArgumentException("Project can't be null");
-        String projectName = project.getName();
-        if(projectName == null || projectName.trim().isEmpty() )
-            throw new IllegalArgumentException("Project name cant be empty");
-        if(projectRepo.getProjectByName(projectName).isPresent())
-            throw new IllegalArgumentException("A project with this name already exists");
-        if(userRepo.getUserByName(project.getCreatedBy()).isEmpty())
-            throw new IllegalArgumentException("A project cannot be created by a nonexistent user");
-        return projectRepo.addProject(project);
+    public int createProjectAndAssignOwner(Project project){
+        projectValidation(project);
+        User owner = userRepo.getUserById(project.getCreatorId()).orElseThrow(
+                () -> new IllegalArgumentException("A project cannot be created by a nonexistent user"));
+        try(Connection connection = DatabaseConnectionManager.getInstance().getConnection()){
+            connection.setAutoCommit(false);
+            try {
+                int newProjectId = projectRepo.addProject(project, connection);
+                if(newProjectId == -1){
+                    throw new SQLException("Failed to generate project ID");
+                }
+                ProjectMembers newMember = new ProjectMembers(newProjectId, owner.getId());
+                boolean memberAdded = projectMembersRepo.addProjectMembers(newMember, connection);
+
+                if(!memberAdded){
+                    throw new SQLException("Failed to add project member");
+                }
+                connection.commit();
+                return newProjectId;
+            } catch (SQLException e){
+                connection.rollback();
+                throw new RuntimeException("Transaction failed and was rolled back.");
+            }
+        } catch (SQLException e){
+            throw new RuntimeException("Database connection error." + e);
+        }
     }
 
     public Project getProjectDetails(int id){
@@ -37,5 +61,17 @@ public class ProjectService {
         if(id < 1)
             return false;
             return projectRepo.deleteProjectById(id);
+    }
+
+    private void projectValidation(Project project){
+        if(project == null)
+            throw new IllegalArgumentException("Project can't be null");
+        String projectName = project.getName();
+        if(projectName == null || projectName.trim().isEmpty() )
+            throw new IllegalArgumentException("Project name cant be empty");
+        if(projectRepo.getProjectByName(projectName).isPresent())
+            throw new IllegalArgumentException("A project with this name already exists");
+        if(userRepo.getUserById(project.getCreatorId()).isEmpty())
+            throw new IllegalArgumentException("A project cannot be created by a nonexistent user");
     }
 }
